701cd36fbe9ecfe42867eaf5525418641fee5d6b
Matthew Deloughry <matt@ledgardjepson.com>
2017-06-30 15:11:54 +0100

    added webpack

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -5,17 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Welcome @yield('title')</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="<?php echo app('url')->asset('css/style.css') ?>">
-        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
         <script type="text/javascript" src="<?php echo app('url')->asset('js/script.js') ?>"></script>
-        
     </head>
     <body class="container">
         <div class="row">
             <div class="col-md-12">
-                <img src="/images/logo.png" width="300px"/>
+                <img src="/images/logo.gif" class="logo"/>
                 @section('body')
                 @show
             </div>
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const webpack = require('webpack');
+const path = require('path');
+var CompressionPlugin = require("compression-webpack-plugin");
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './app.js',
+  output: {
+    filename: 'script.js',
+    path: path.resolve(__dirname, 'public/js')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      { test: /\.scss$/,
+        loaders: [
+          'style',
+          'css',
+          'sass'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+       }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      Tether: 'tether',
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false,
+            properties: true,
+        sequences: true,
+        dead_code: true,
+        conditionals: true,
+            comparisons: true,
+            evaluate: true,
+        booleans: true,
+        unused: true,
+            loops: true,
+            hoist_funs: true,
+            cascade: true,
+        if_return: true,
+        join_vars: true,
+        drop_console: true,
+            drop_debugger: true,
+            negate_iife: true,
+            unsafe: true,
+            hoist_vars: true,
+            //side_effects: true
+        },
+        sourceMap: true,
+        comments: false,
+        mangle: {
+          // Skip mangling these
+          except: ['$super', '$', 'exports', 'require']
+        }
+    }),
+    new webpack.optimize.MinChunkSizePlugin({
+      minChunkSize: 10000 // Minimum number of characters
+    })
+  ]
+}
